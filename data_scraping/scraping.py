da5eed55975a559ca9d92f39ef241f594a6ba834
Xinyi Zhu <juliezhu212@outlook.com>
2021-11-06 00:05:18 -0500

    Set up FacebookPostsScraper

---
+++ data_scraping/scraping.py
@@ -0,0 +1,19 @@
+from FacebookPostsScraper import FacebookPostsScraper as Fps
+from pprint import pprint as pp
+import login
+
+# Enter your Facebook email and password
+email = login.EMAIL
+password = login.PASSWORD
+
+# Instantiate an object
+fps = Fps(email, password, post_url_text='Full Story')
+
+# Example with single profile
+single_profile = 'https://www.facebook.com/BillGates'
+data = fps.get_posts_from_profile(single_profile)
+pp(data)
+
+fps.posts_to_csv('my_posts')  # You can export the posts as CSV document
+# fps.posts_to_excel('my_posts')  # You can export the posts as Excel document
+# fps.posts_to_json('my_posts')  # You can export the posts as JSON document
